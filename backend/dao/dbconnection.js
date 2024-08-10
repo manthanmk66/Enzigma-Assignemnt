ef73d499b95b3b84aeb97c0ff2aaa538dac92206
Manthan Kasle <manthankaslemk@gmail.com>
2024-08-10 15:01:41 +0530

    Added Nodejs Backend

---
+++ backend/dao/dbconnection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//database Connection with MongoDB
+
+const connectDB = async () => {
+  console.log(process.env.MONGO_URL);
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
