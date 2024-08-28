fb200c7e14ac28b0411e33b40d53979de0e79587
Manthan Kasle <manthankaslemk@gmail.com>
2024-08-28 14:44:12 +0530

    chore:

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DeleteModal = ({ confirmDelete, cancelDelete }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center  justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-6 shadow-md">
         <h2 className="text-lg font-semibold mb-4">Confirm Deletion</h2>
         <p>Are you sure you want to delete this task?</p>
